a82b6d98378534ab931eb94c0a9cda03e151cfd8
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-01-21 16:00:17 +0100

    Edit description in file

--- r-programming/assignment-2/cachematrix.R
+++ r-programming/assignment-2/cachematrix.R
@@ -1,9 +1,9 @@
 ## The two functions, makeCacheMatrix and cacheSolve, solve, store, return the
 ## inverse of a matrix. In order to save computation time, the inverse matrix is
-## stores in the cache for fast recall.
+## stores in the cache for later recalls.
 
-## Initialize a set of functions to get and set the matrix x or its inverse. The
-## function return a list which can be passed to the cacheSolve function.
+## Initialize a set of functions to 'get' and 'set' the matrix x or its inverse.
+## The function return a list which can be passed to the cacheSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
@@ -21,9 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The function cacheSolve return the inverse of a matrix from the cache if it
+## The function cacheSolve return the inverse of a matrix, from the cache, if it
 ## exist. If not, it calculate it, store it, and returning it. cacheSolve takes
-## an argument, created by the makeCacheMatrix function.
+## an argument, x, created by the makeCacheMatrix function.
 
 cacheSolve <- function(x, ...) {
         inverse <- x$getinverse()
