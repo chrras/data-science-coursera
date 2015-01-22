bd6a740840a5f270315ca1e6c72f1c3967aadcf1
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-01-22 17:12:54 +0100

    First working function

--- r-programming/assignment-3/rankhospital.R
+++ r-programming/assignment-3/rankhospital.R
@@ -1,5 +1,5 @@
 rankhospital <- function(state, outcome, num = "best") {
-
+        
         ## Read outcome data
         
         data <- read.csv("data/outcome-of-care-measures.csv",
@@ -21,12 +21,23 @@ rankhospital <- function(state, outcome, num = "best") {
         
         ## Find outcome collumn number
         
+        # idx <- match(outcome, outcomeVec)
         idx <- match(outcome, outcomeVec)
         col <- as.integer(names(outcomeVec[idx]))
         
+        ## Subset and sort data
         
+        data <- subset(data, State == state)    ## Subset
+        data[, col] <- as.numeric(data[, col])  ## Convert outcome to numeric
+        data <- data[!is.na(data[, col]), ]     ## Remove NAs
+        data <- data[order(data[, col], data$Hospital.Name), ]  ## Sort data (lo -> hi)
+        # data <- data[order(data[, col]), ]  ## Sort data (lo -> hi)
         
-        
-        ## Return hospital name in that state with the given rank
-        ## 30-day death rate
+        if (num == 'best') {
+                return(data[1, 2])
+        } else if (num == 'worst') {
+                return(tail(data[, 2], 1))
+        } else {
+                return(data[num, 2])
+        }
 }
