982000698c9d85d324a555258e0fc8467d03e6a7
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-02-05 14:33:54 +0100

    Add comments

--- exploratory-data-analysis/assignment-1/loading-and-plotting.R
+++ exploratory-data-analysis/assignment-1/loading-and-plotting.R
@@ -18,9 +18,10 @@ if (!file.exists('household_power_consumption.txt')) {
 
 if (!file.exists('household_power_consumption_subset.txt')) {
         
+        ## Create header in new subset file via Terminal (Mac)
         system("head -n 1 household_power_consumption.txt > household_power_consumption_subset.txt")
         
-        
+        ## Append lines in original data with the right date via Terminal (Mac)
         system("grep ^[1-2]/2/2007 household_power_consumption.txt >> household_power_consumption_subset.txt")
 }
 
