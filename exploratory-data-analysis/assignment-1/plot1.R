5b9ee5701783da9c899046d0822598318b7d3cb6
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-02-05 16:48:28 +0100

    Add comments

--- exploratory-data-analysis/assignment-1/plot1.R
+++ exploratory-data-analysis/assignment-1/plot1.R
@@ -1,29 +1,32 @@
-## Load required functions ----
+## Load required functions -----------------------------------------------------
 
 setwd(paste('/Users/Christoffer/data-science-coursera/',
             'exploratory-data-analysis/assignment-1', sep = ""))
 
 source('load_data.R')
 
-## Load data ----
+
+## Load data -------------------------------------------------------------------
 
 data <- load_data()  # See the load_data.R file for details
 
 
-## Create directory 'plots' if it doesn't exists ----
+## Create directory 'plots' if it doesn't exists -------------------------------
 
 if(!file.exists('plots')) {
     dir.create('plots')
 }
 
 
-## Create histogram ----
+## Plotting  -------------------------------------------------------------------
 
+## Set png options
 png('plots/plot1.png',
     width = 480,
     height = 480,
     units = "px")
 
+## Create plot
 hist(data$Global_active_power,
      main = 'Global Active Power',
      xlab = 'Global Active Power (kilowatts)',
