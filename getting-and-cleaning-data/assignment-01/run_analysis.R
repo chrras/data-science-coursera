9f6ad62ea42ad350abe7a0c25c04abfe941a170b
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-02-18 12:53:11 +0100

    Add comments: run_analysis.R

--- getting-and-cleaning-data/assignment-01/run_analysis.R
+++ getting-and-cleaning-data/assignment-01/run_analysis.R
@@ -1,44 +1,45 @@
 run_analysis <- function() {
 
-    ## You should create one R script called run_analysis.R that does the
-    ## following:
+#     require(dplyr)
     
-    require(dplyr)
-    
-    ## 1.   Merges the training and the test sets to create one data set.
     setwd('data-gitignore')
+    
+    ## Load header and activity labels
     header <- read.table('features.txt', colClasses = 'character')
     labels <- read.table('activity_labels.txt', colClasses = 'character')
     
+    ## Load data for training and test data sets
     for(i in c('train', 'test')) {
+        ## Load measurements
         data <- read.table(paste0(i, '/X_', i, '.txt'), colC = 'numeric')
+        ## Load activity collumn
         activity <- read.table(paste0(i, '/y_', i,'.txt'), colC = 'character')
+        ## Load subject collumn
         subject <- read.table(paste0(i, '/subject_', i,'.txt'), colC = 'factor')
     
+        ## Combine data (first loop: training data, second loop: test data)
         assign(i, cbind(subject, activity, rep(i, nrow(subject)), data))
     }
     
+    ## Merges the training and the test sets to create one data set
     data <- rbind(train, test)
     colnames(data) <- c('subject', 'activity', 'case', header[, 2])
 
-    ## 2.   Extracts only the measurements on the mean and standard
-    ##      deviation for each measurement.
-    
+    ## Extracts collumn names ending on 'mean()' or 'std()'
     data <- data[, grepl('mean()', colnames(data[-(1:3)]), fixed = TRUE) |
                      grepl('std()', colnames(data[-(1:3)]), fixed = TRUE)]
     
-    ## Uses descriptive activity names to name the activities in the data set.
-    
-    labels[, 2] <- tolower(labels[, 2])
-    
+    ## Create descriptive activity names
     for (i in 1:nrow(labels)) {
         data$activity <- gsub(as.character(i), labels[i, 2], data$activity)
     }
+    ## convert text to lower case
+    data$activity <- tolower(data$activity)
+
+    ## Appropriately labels the data set with descriptive variable names
     
-    chartr('123', 'ABC', c(1,2,3,4,5))
-    ?sub
-    ## 4.   Appropriately labels the data set with descriptive variable
-    ##      names. 
+    
+    
     ## 5.   From the data set in step 4, creates a second, independent tidy
     ##      data set with the average of each variable for each activity and each subject.
 
