87ac28ad660ff5e6be6f01863cd3392e1476fdc8
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-02-18 13:54:16 +0100

    Bug fix: collumn extraction. Add feature: rearrange data

--- getting-and-cleaning-data/assignment-01/run_analysis.R
+++ getting-and-cleaning-data/assignment-01/run_analysis.R
@@ -1,6 +1,7 @@
 run_analysis <- function() {
 
-#     require(dplyr)
+    require(reshape2)
+    require(dplyr)
     
     setwd('data-gitignore')
     
@@ -8,44 +9,40 @@ run_analysis <- function() {
     header <- read.table('features.txt', colClasses = 'character')
     labels <- read.table('activity_labels.txt', colClasses = 'character')
     
-    ## Load data for training and test data sets
+    ## Load data for train and test data sets (measurements, activity, subject)
     for(i in c('train', 'test')) {
-        ## Load measurements
-        data <- read.table(paste0(i, '/X_', i, '.txt'), colC = 'numeric')
-        ## Load activity collumn
+        measurement <- read.table(paste0(i, '/X_', i, '.txt'), colC = 'numeric')
         activity <- read.table(paste0(i, '/y_', i,'.txt'), colC = 'character')
-        ## Load subject collumn
-        subject <- read.table(paste0(i, '/subject_', i,'.txt'), colC = 'factor')
-    
-        ## Combine data (first loop: training data, second loop: test data)
-        assign(i, cbind(subject, activity, rep(i, nrow(subject)), data))
+        subject <- read.table(paste0(i, '/subject_', i,'.txt'), colC = 'integer')
+        
+        ## Combine and name data (1st loop: training data, 2nd loop: test data).
+        ## A 3rd collumn is create to indicate if the data is from train or test
+        assign(i, cbind(subject, activity, rep(i, nrow(subject)), measurement))
     }
     
     ## Merges the training and the test sets to create one data set
     data <- rbind(train, test)
+    
+    ## Add descriptive header
     colnames(data) <- c('subject', 'activity', 'case', header[, 2])
 
     ## Extracts collumn names ending on 'mean()' or 'std()'
-    data <- data[, grepl('mean()', colnames(data[-(1:3)]), fixed = TRUE) |
-                     grepl('std()', colnames(data[-(1:3)]), fixed = TRUE)]
+    data <- cbind(data[, 1:3], data[, grepl('mean()', colnames(data), fixed = TRUE) |
+                     grepl('std()', colnames(data), fixed = TRUE)])
     
     ## Create descriptive activity names
     for (i in 1:nrow(labels)) {
         data$activity <- gsub(as.character(i), labels[i, 2], data$activity)
     }
-    ## convert text to lower case
-    data$activity <- tolower(data$activity)
-
-    ## Appropriately labels the data set with descriptive variable names
+    data$activity <- tolower(data$activity)  # Make lower case
     
+    ## Creates a independent data set with the average of each variable for each
+    ## activity and each subject.
+    data_melt <- melt(data, id.vars = c("subject", "activity", "case"))
+    data_cast <- dcast(data_melt, subject + activity ~ variable, mean)
+    data_cast <- arrange(data_cast, activity, subject)
     
+    setwd('..')
     
-    ## 5.   From the data set in step 4, creates a second, independent tidy
-    ##      data set with the average of each variable for each activity and each subject.
-
+    return(data_cast)
 }
-
-setwd('data-gitignore')
-folder <- 'test'
-test_data <- read.table('test/X_test.txt'), colClasses = 'numeric')
-header <- read.table('X_test.txt', header = F, colClasses = 'numeric')
