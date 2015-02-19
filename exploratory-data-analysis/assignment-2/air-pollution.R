5d845a33e98b3a25e656ed926dfabe229d336a0d
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2015-02-19 15:22:20 +0100

    Add task 6

--- exploratory-data-analysis/assignment-2/air-pollution.R
+++ exploratory-data-analysis/assignment-2/air-pollution.R
@@ -117,3 +117,37 @@ plot(yearlyEmission,
      xaxt='n',
      main = expression(paste("Development of ", PM[2.5], " motor vehicle emission over time in Baltimore City")))
 axis(side=1, at=c(1999, 2002, 2005, 2008))
+
+## Task 6 ----
+
+## Aggregate data
+
+NEI_Baltimore_LA <- filter(NEI, fips %in% c("24510", "06037"), type == 'ON-ROAD')
+yearlyEmission <- aggregate(NEI_Baltimore_LA$Emission, by = list(NEI_Baltimore_LA$year, NEI_Baltimore_LA$fips), FUN = sum)
+colnames(yearlyEmission) <- c('Year', 'Fips', 'Emission')
+yearlyEmission$Emission <- yearlyEmission$Emission / 1000
+yearlyEmission$EmissionDiff <- c(yearlyEmission$Emission[1:4] - yearlyEmission$Emission[1],
+                                 yearlyEmission$Emission[5:8] - yearlyEmission$Emission[5])
+
+yearlyEmission$Fips <- factor(yearlyEmission$Fips)
+levels(yearlyEmission$Fips) <- c('Los Angeles County', 'Baltimore City')
+
+## Plot PM2.5 emission development in Baltimore Ciry
+ggplot(data = yearlyEmission, aes(x = Year, y = Emission)) +
+    geom_line() +
+    geom_point() +
+    scale_x_continuous(breaks = c(1999, 2002, 2005, 2008), labels = c('`99', '`02', '`05', '`08')) +
+    ylab(expression(paste(PM[2.5], " emission [", 10^3, " tons]"))) +
+    facet_grid(. ~ Fips) +
+    labs(title = "Development of PM2.5 motor vehicle emission over time\n") +
+    theme_bw(base_size = 14)
+
+## Plot PM2.5 emission development in Baltimore Ciry
+ggplot(data = yearlyEmission, aes(x = Year, y = EmissionDiff)) +
+    geom_line() +
+    geom_point() +
+    scale_x_continuous(breaks = c(1999, 2002, 2005, 2008), labels = c('`99', '`02', '`05', '`08')) +
+    ylab(expression(paste("Change in ", PM[2.5], " emission [", 10^3, " tons]"))) +
+    facet_grid(. ~ Fips) +
+    labs(title = "Development of PM2.5 motor vehicle emission over time\n") +
+    theme_bw(base_size = 14)
